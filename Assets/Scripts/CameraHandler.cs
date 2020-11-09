2b4c5520a8ed77773ba0a36e842c3ae787602da5
Seytn <camillo18@gmail.com>
2020-11-10 00:54:03 +0100

    dead wife kills you

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,42 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    public CinemachineVirtualCamera sourceCam;
+    public CinemachineVirtualCamera targetCam;
+    public CinemachineVirtualCamera endCam;
+
+    private int sourceCamPriority;
+
+    int activePriority = 11;
+    int notActivePriority = 9;
+
+    public void ActivateSourceCamera()
+    {
+        sourceCam.Priority = activePriority;
+        targetCam.Priority = notActivePriority;
+        if (endCam != null)
+            endCam.Priority = notActivePriority;
+
+    }
+
+    public void ActivateTargetCamera()
+    {
+        sourceCam.Priority = notActivePriority;
+        targetCam.Priority = activePriority;
+        if (endCam != null)
+            endCam.Priority = notActivePriority;
+    }
+
+    public void ActivateEndCamera()
+    {
+        if (endCam == null) return;
+
+        sourceCam.Priority = notActivePriority;
+        targetCam.Priority = notActivePriority;
+        endCam.Priority = activePriority;
+    }
+}
--- Assets/Scripts/Hatch.cs
+++ Assets/Scripts/Hatch.cs
@@ -6,6 +6,10 @@ public class Hatch : MonoBehaviour
 {
     public Animator animator;
     public HatchKey item;
+    public CameraHandler cameraHandler;
+    public PlayerController playerController;
+    public List<GameObject> objectToActivate;
+    public List<GameObject> objectToDeactivate;
 
     IInventory inventory;
 
@@ -20,6 +24,38 @@ public class Hatch : MonoBehaviour
         {
             animator.SetTrigger("openHatch");
             inventory.RemoveItem(item, Inventory.ItemType.KEY);
+            ActivateItems();
+            playerController.IsImmobilized = true;
+            cameraHandler.ActivateTargetCamera();
+            DeactivateItems();
+            StartCoroutine(ResetCamera());
         }
+    }
+
+    void ActivateItems()
+    {
+        foreach (GameObject obj in objectToActivate)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    void DeactivateItems()
+    {
+        foreach (GameObject obj in objectToDeactivate)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    private IEnumerator ResetCamera()
+    {
+        yield return new WaitForSeconds(1f);
+        cameraHandler.ActivateEndCamera();
+        playerController.gameObject.transform.Rotate(0f, 180f, 0f);
+        playerController.animator.SetTrigger("blinded");
+        yield return new WaitForSeconds(4f);
+        playerController.IsImmobilized = false;
+        cameraHandler.ActivateSourceCamera();
     }
 }
