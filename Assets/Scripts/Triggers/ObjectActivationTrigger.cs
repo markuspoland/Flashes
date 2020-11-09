9e0062856085de4b2f9448ca3dd1e61c74b6be34
Seytn <camillo18@gmail.com>
2020-11-09 23:58:49 +0100

    running zombies trigger

---
+++ Assets/Scripts/Triggers/ObjectActivationTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectActivationTrigger : MonoBehaviour
+{
+
+    public List<GameObject> objectToActivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (GameObject obj in objectToActivate)
+            {
+                obj.SetActive(true);
+            }
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/ZombieRunTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieRunTrigger : MonoBehaviour
+{
+    public CameraHandler cameraHandler;
+    public PlayerController playerController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerController.IsImmobilized = true;
+            cameraHandler.ActivateTargetCamera();
+            StartCoroutine(ResetCamera());
+        }
+    }
+
+    private IEnumerator ResetCamera()
+    {
+        yield return new WaitForSeconds(3f);
+        cameraHandler.ActivateSourceCamera();
+        yield return new WaitForSeconds(2.3f);
+        playerController.IsImmobilized = false;
+    }
+}
