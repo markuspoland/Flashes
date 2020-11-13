f77d7b6755bd2646698dd0afccd36b164b942dfb
Marek <markuspoland@gmail.com>
2020-11-13 16:29:05 +0100

    Monster RUUUUUUN!!!!!

---
+++ Assets/WallDestroyTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDestroyTrigger : MonoBehaviour
+{
+    public DestroyWall wall;
+    public Animator playerAnim;
+    public PlayerController playerController;
+    public GameObject monster;
+    public CameraHandler cameraHandler;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerController.IsImmobilized = true;
+            playerController.ShowActionCamera();
+            wall.WallDestroy();
+            monster.SetActive(true);
+            playerAnim.SetTrigger("Block");
+            StartCoroutine(MonsterScream());
+            
+        }
+    }
+
+    IEnumerator MonsterScream()
+    {
+        yield return new WaitForSeconds(1f);
+        cameraHandler.ActivateTargetCamera();
+        yield return new WaitForSeconds(2f);
+        cameraHandler.ActivateSourceCamera();
+        playerController.IsImmobilized = false;
+        Destroy(gameObject);
+    }
+}
