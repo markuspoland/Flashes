04c70c0d47bcde65c066a18d96e8d5aef13e5a99
Seytn <camillo18@gmail.com>
2020-11-13 13:24:52 +0100

    basement with probuilder

---
+++ Assets/DestroyWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWall : MonoBehaviour
+{
+    Rigidbody[] bricks;
+
+    void Start()
+    {
+        bricks = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody brick in bricks)
+        {
+            brick.isKinematic = false;
+            brick.AddForce(Vector3.forward * 200, ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
