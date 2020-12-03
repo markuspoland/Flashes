30bd0afb911f96a6f058f95241026a293ae0ff1d
Seytn <camillo18@gmail.com>
2020-12-03 14:59:12 +0100

    morgue door sound

---
+++ Assets/Scripts/Interactions/MorgueDoors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorgueDoors : MonoBehaviour
+{
+    public List<Animator> doorAnimators;
+    public AudioClip doorOpen;
+
+    AudioSource audioSource;
+    InteractibleObject interactible;
+
+    void Start()
+    {
+        interactible = GetComponent<InteractibleObject>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (interactible.CanInteract() && Input.GetKeyDown(KeyCode.E))
+        {
+            audioSource.PlayOneShot(doorOpen);
+            interactible.Disable();
+            foreach (Animator anim in doorAnimators)
+            {
+                anim.enabled = true;
+            }
+        }
+    }
+}
